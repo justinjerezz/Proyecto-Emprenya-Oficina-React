bd8b8cb6449466c7529046adfdb7609d4ed98956
Justin Jerez <76711971+justinjerezz@users.noreply.github.com>
2022-10-27 17:41:00 +0200

    Codigo Depurado

--- emprenya-oficina/src/components/formLogin.js
+++ emprenya-oficina/src/components/formLogin.js
@@ -1,60 +1,11 @@
 // Render Prop
-import React, { useState } from 'react';
-import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
-import {validarLogin} from '../servicios/validarLogin'
-export default function Formulario(){
-    const [username,setUsername]=useState('');
-    const [password,setPassword]=useState('');
+import React from 'react';
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        validarLogin();
-    }
+export default function Formulario(){
 
 return(
-    <Form className='bg-login login' onSubmit={handleSubmit}>
-        <div className='imagenEmprenya'>
-        <img src={require('../assets/imagenes/emprenya.png')} alt="" />
-        </div>
-        <FormGroup>
-            <Label
-                for="exampleEmail"
-                className='labelLogin'
-            >
-                Email
-            </Label>
-            <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="Email"
-                type="text"
-                onChange={(e)=>setUsername(e.target.value)}
-                value={username}
-                className='form-control'
-            />
-        </FormGroup>
-        {' '}
-        <FormGroup>
-            <Label
-                for="examplePassword"
-                className='labelLogin'
-            >
-                Password
-            </Label>
-            <Input
-                id="examplePassword"
-                name="password"
-                placeholder="Password"
-                type="password"
-                onChange={(e)=>setPassword(e.target.value)}
-                value={password}
-                className='form-control'
-            />
-        </FormGroup>
-        {' '}
-        <Button className='botonIniciar'>
-            Iniciar
-        </Button>
-    </Form>
+    <div className='containerFormulario'>
+        
+    </div>
 );
 }
