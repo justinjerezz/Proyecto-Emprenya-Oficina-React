f2bd8ffbcf6f6896b2319ea5e33ac96d0204faad
Justin Jerez <76711971+justinjerezz@users.noreply.github.com>
2022-10-27 18:25:27 +0200

    Empiezo Formulario Login

--- emprenya-oficina/src/components/formLogin.js
+++ emprenya-oficina/src/components/formLogin.js
@@ -1,11 +1,39 @@
 // Render Prop
 import React from 'react';
 
-export default function Formulario(){
+/*Estilos de componente*/
 
-return(
-    <div className='containerFormulario'>
-        
-    </div>
-);
+
+import "../assets/css/FormularioLogin/formularioLogin.css";
+
+export default function Formulario() {
+
+    return (
+        <div className='containerGeneral'>
+            <div className='cajaFormulario'>
+                <div className='containerFormulario'>
+                    <div>
+                        <img src={require(`../assets/imagenes/emprenya.png`)} alt="Logo-Empren-Ya" className='img-empren-ya' />
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Email o usuario' className='input-usuario' />
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Contraseña'  className='input-pass'/>
+                    </div>
+                    <div>
+                        <div>
+                            <button className='boton-inicio-sesion'></button>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <button>INICIAR SESIÓN</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
